12a20bc350c342481885fbad8fa04a072f18aca2
MunoLike <MunoLike>
2021-12-28 09:16:40 +0900

    add asm_sum2.c

--- assembly/asm_sum2.c
+++ assembly/asm_sum2.c
@@ -4,25 +4,14 @@ void main(void){
     short sum = 0;
     printf("sum=%d\n", sum);
 
-    
-    /*
-    short a = 0;
-    short b = 1;
-
-    for(;b < 11;b++){
-        a += b;
-    }
-    sum = a;
-    */
     __asm__ __volatile__(
         "mov ax, 0;"
         "mov bx, 1;"
-        "LOOP1: cmp bx, 11;"
-        "je MOUT;"
-        "add ax, bx;"
+"LOOP1:  add ax, bx;"
         "inc bx;"
-        "jmp LOOP1;"
-        "MOUT: mov %[sum], ax;"
+        "cmp bx, 11;"
+        "jne LOOP1;"
+        "mov %[sum], ax;"
         : [sum]"=r"(sum)
 
         
