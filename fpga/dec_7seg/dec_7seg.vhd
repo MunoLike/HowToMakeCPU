8beb1847e8b4c2f5c9adb8b79ae877f4aaee2aa0
MunoLike <sukorice@gmail.com>
2022-01-05 20:16:51 +0900

    fix dec_7seg.vhd SEG7 vector width

--- fpga/dec_7seg/dec_7seg.vhd
+++ fpga/dec_7seg/dec_7seg.vhd
@@ -3,7 +3,7 @@ use IEEE.std_logic_1164.all;
 entity dec_7seg is
     port (
         DIN  : in std_logic_vector(3 downto 0);
-        SEG7 : out std_logic_vector(7 downto 0)
+        SEG7 : out std_logic_vector(6 downto 0)
     );
 end dec_7seg;
 
